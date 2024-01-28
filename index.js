43981c8d259a23d8ee6b8924a9c19360128418af
Yukitoshi Imaizumi-Zhou <yukitoshi.imaizumizhou@gmail.com>
2024-01-28 23:20:24 +1100

    completed index.js

--- index.js
+++ index.js
@@ -6,58 +6,58 @@ const generateMarkdown = require('./utils/generateMarkdown')
 const questions = [
     
     {
-        type: 'input',
         name: 'Github Username',
         message: 'What is your GitHub username?',
+        type: 'input'
     },
 
     {
-        type: 'input',
         name: 'Email',
         message: 'What is your contact email?',
+        type: 'input'
     },
 
     {
-        type: 'input',
         name: 'Title',
         message: 'What title do you want to give this project?',
+        type: 'input',
     },
 
     {
-        type: 'input',
         name: 'Description',
         message: 'Tell me in your own words, what this project is about!',
+        type: 'input'
     },
 
     {
-        type: 'input',
         name: 'Installation Instructions',
         message: 'Walk me through the installation instruction someone needs to take to set up this project.',
+        type: 'input'
     },
 
     {
-        type: 'input',
         name: 'Usage Information',
         message: 'Provide the use of this project.',
+        type: 'input'
     },
 
     {
-        type: 'input',
         name: 'Contribution Guidelines',
         message: 'How can users contribute to this project?',
+        type: 'input'
     },
 
     {
-        type: 'input',
         name: 'Test Instructions',
         message: 'Provide instructions to test this project.',
+        type: 'input'
     },
 
     {
-        type: 'list',
         name: 'License',
         message: 'Choose a License for this project',
         choices: ['MIT', 'Apache 2.0', 'Boost 1.0', 'BSD 2', 'BSD 3', 'CCZ 1.0', 'GPL 2.0', 'GPL 2.1', 'GPL 3.0', 'Affero GPL 3.0', 'EPL 2.0', 'MPL 2.0', 'The Unlicense', 'None'],
+        type: 'list'
     },
 ];
 
@@ -74,7 +74,7 @@ function init() {
         .then((answers) => {
             writeToFile("README.md", generateMarkdown(answers));
         })
-        .catch((err) => console.error('Failed to generate a README file: ', err));
+        .catch((error) => console.error('Failed to generate a README file: ', error));
 }
 
 // Function call to initialize app
